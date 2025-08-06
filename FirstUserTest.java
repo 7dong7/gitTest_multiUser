43164c64b9fb1e23acf41f9f4723b3d640bdfa9d
= <=>
2025-08-06 21:43:21 +0900

    user1 config set

--- FirstUserTest.java
+++ FirstUserTest.java
@@ -11,5 +11,7 @@ public class FirstUserTest {
         String edit2 = "user2 Edit to 11 line";
 
         String edit = "user1 Edit to 11 line";
+
+        String user1 = "user1 config set";
     }
 }
