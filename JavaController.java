e774d3afd239c39eead651b6a0fd4de235c82384
user1 <=>
2025-08-06 22:03:55 +0900

    javaController dont delete

---
+++ JavaController.java
@@ -0,0 +1,10 @@
+public class JavaController {
+
+    public static void main(String[] args) {
+
+        String java = "create java Controller";
+
+        String edit = "this javaController edit";
+
+    }
+}
